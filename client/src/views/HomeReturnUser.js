fd2be827eeb04b1dcb42130911b54225fc6abcec
ZeroJimmy100 <61480298+ZeroJimmy100@users.noreply.github.com>
2020-05-22 11:51:02 -0700

    Create HomeReturnUser.js

---
+++ client/src/views/HomeReturnUser.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Tabs, Tab } from 'react-mdl';
+
+class HomeReturnUser extends Component {
+   
+
+    render() {
+        return(
+            <div className="category-tabs">
+                <h1> WELCOME BACK! user</h1>
+                
+            </div>
+        )
+    }
+}
+
+export default HomeReturnUser;
