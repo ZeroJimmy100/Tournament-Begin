2860a02ee5d0a8d8445e2b188674d7a3aa382d53
ZeroJimmy100 <61480298+ZeroJimmy100@users.noreply.github.com>
2020-05-22 12:27:15 -0700

    Update ForumList.js

---
+++ client/src/components/ForumList.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from '@reach/router';
+import axios from 'axios';
+import DeleteButton from './DeleteButton';
+
+
+export default props => {
+    
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
