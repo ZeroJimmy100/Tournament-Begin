95e6969910675f00cc4ed6ada407d75342215c69
ZeroJimmy100 <61480298+ZeroJimmy100@users.noreply.github.com>
2020-05-22 16:54:48 -0700

    Update Home.js

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -10,12 +10,27 @@ class Home extends Component {
             <div className="category-tabs">
                 <div className="background">
                     <h1> WELCOME! user</h1>
-                    <div className="TopThreeTourneyBox">
-                        <h4 id="TopThree">Top Three Tournament</h4>
+                    <div className="TopPart">
+                        <div className="TopThreeTourneyBox">
+                            <h4 id="TopThree">Top Three Tournament</h4>
+                        </div>
+                        <div className="MyTourneyBox">
+                            <h4 id="MyTournament">My Tournament</h4>
+                        </div>
+                        <div className="FriendBox">
+                            <h4 id="Friends">Friend list</h4>
+                        </div>
                     </div>
                     <div className="TopThreePrizeTournament">
                         <h4 id="TopPrize">Top Three Prize Tournament</h4>
                     </div>
+                    <div className="OngoingTournamentBox">
+                        <h4 id="OngoingTourney">Ongoing Tournaments</h4>
+                    </div>
+                    <div className="RecentTourneyPost">
+                        <h4 id="Recent">Recent Tourney</h4>
+                    </div>
+                    
                 </div>
                 
             </div>
