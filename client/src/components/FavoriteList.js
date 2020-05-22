0acb1bdf80072fc0945856edab2f48984a6e3657
ZeroJimmy100 <61480298+ZeroJimmy100@users.noreply.github.com>
2020-05-22 12:27:13 -0700

    Update FavoriteList.js

---
+++ client/src/components/FavoriteList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from '@reach/router';
+import axios from 'axios';
+
+
+export default props => {
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
