31bded09868d30c738b2f70b2bcd04e2ce923424
Chrisna <60117964+chrisnaly@users.noreply.github.com>
2020-05-21 18:17:24 -0700

    Navbar/tabs

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Router } from '@reach/router';
+import { Router, Link } from '@reach/router';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import Home from './views/Home';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-          
-      </Router>
-    </div>
+    <Layout>
+        <Header title={<Link style={{textDecoration: 'none', color: 'white'}} to="/"> <i class="fa fa-trophy" aria-hidden="true" width="50"> Tournament Begin </i></Link>} scroll>
+            <Navigation>
+              <a href="/">Contact</a>
+              <a href="/">Support</a>
+              <a href="/">Account</a>
+              <a href="/">Link</a>
+            </Navigation>
+        </Header>
+        <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/"> <i class="fa fa-trophy" aria-hidden="true" width="50"> Home </i></Link>}>
+            <Navigation>
+              <a href="/">Contact</a>
+              <a href="/">Support</a>
+              <a href="/">Account</a>
+              <a href="/">Link</a>
+            </Navigation>
+        </Drawer>
+        <Content>
+            <div className="page-content">
+              <Router>
+                <Home path="/" />
+              </Router>
+            </div>
+        </Content>
+    </Layout>
+</div>
   );
 }
 
