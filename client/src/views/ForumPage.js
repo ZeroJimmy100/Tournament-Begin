341fc513898d0e6cb55bc02ed1553fbc2d8661c2
ZeroJimmy100 <61480298+ZeroJimmy100@users.noreply.github.com>
2020-05-22 11:50:58 -0700

    Update ForumPage.js

--- client/src/views/ForumPage.js
+++ client/src/views/ForumPage.js
@@ -1,9 +1,67 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { Tabs, Tab } from 'react-mdl';
 
-export default () => {
-    return (
-        <div>
-            
-        </div>
-    )
+class FourmPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { activeTab: 0 };
+    }
+
+    toggleCategories() {
+        if(this.state.activeTab == 0) {
+            return(
+                <div>
+                    <h1>Tab One</h1>
+                </div>
+            )
+        }
+        else if(this.state.activeTab == 1) {
+            return(
+                <div>
+                    <h1>Tab Two</h1>
+                </div>
+            )
+        }
+        else if(this.state.activeTab == 2) {
+            return(
+                <div>
+                    <h1>Tab Three</h1>
+                </div>
+            )
+        }
+        else if(this.state.activeTab == 3) {
+            return(
+                <div>
+                    <h1>Tab Four</h1>
+                </div>
+            )
+        }
+        else if(this.state.activeTab == 4) {
+            return(
+                <div>
+                    <h1>Tab Five</h1>
+                </div>
+            )
+        }
+    }
+
+    render() {
+        return(
+            <div className="category-tabs">
+                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
+                    <Tab> One </Tab>
+                    <Tab> Two </Tab>
+                    <Tab> Three </Tab>
+                    <Tab> Four </Tab>
+                    <Tab> Five </Tab>
+                </Tabs>
+
+                <section className="contents">
+                    {this.toggleCategories()}
+                </section>
+            </div>
+        )
+    }
 }
+
+export default FourmPage;
