d6d6ee49b052b00e1af64f48025c59128a3ec3ac
ZeroJimmy100 <61480298+ZeroJimmy100@users.noreply.github.com>
2020-05-22 11:50:54 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import { Router, Link } from '@reach/router';
 import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 import Home from './views/Home';
+import ForumPage from './views/ForumPage';
+import HomeReturnUser from './views/HomeReturnUser';
 
 function App() {
   return (
@@ -13,21 +15,23 @@ function App() {
               <a href="/">Contact</a>
               <a href="/">Support</a>
               <a href="/">Account</a>
-              <a href="/">Link</a>
+              <a href="/Forum-Page">Forum</a>
             </Navigation>
         </Header>
         <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/"> <i class="fa fa-trophy" aria-hidden="true" width="50"> Home </i></Link>}>
             <Navigation>
-              <a href="/">Contact</a>
-              <a href="/">Support</a>
               <a href="/">Account</a>
-              <a href="/">Link</a>
+              <a href="/">My Tournaments</a>
+              <a href="/">Friends</a>
+              <a href="/">My Forum</a>
             </Navigation>
         </Drawer>
         <Content>
             <div className="page-content">
               <Router>
                 <Home path="/" />
+                <HomeReturnUser path="/1" />
+                <ForumPage path="/Forum-Page"/>
               </Router>
             </div>
         </Content>
