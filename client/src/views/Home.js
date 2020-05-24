36d5027484fbc7f870e1ccb629a9cb226af44f9f
Daniel <dslee1993@gmail.com>
2020-05-24 02:48:43 -0700

    Update Home.js

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -10,9 +10,6 @@ class Home extends Component {
         return(
             <div className="category-tabs">
                 <div className="background">
-                <div>
-                    <NavBar/>
-                </div>
                     <h1> WELCOME! Asshole</h1>
 
                     <div className="TopPart">
