b9a392ca7dd443597a8689a796bf3253bc80e858
Daniel <dslee1993@gmail.com>
2020-05-24 01:16:15 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -11,39 +11,13 @@ import LoginRegistration from './views/LoginRegistration';
 function App() {
   return (
     <div className="App">
-    <Layout>
-        <Header title={<Link style={{textDecoration: 'none', color: 'white'}} to="/"> <i class="fa fa-trophy" aria-hidden="true" width="50"> Tournament Begins </i></Link>} scroll>
-            <Navigation>
-              <a href="/">Contact</a>
-              <a href="/">Support</a>
-              <a href="/">Account</a>
-              <a href="/Forum-Page">Forum</a>
-            </Navigation>
-        </Header>
-        <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/"> <i class="fa fa-trophy" aria-hidden="true" width="50"> Home </i></Link>}>
-            <Navigation>
-              <a href="/">Account</a>
-              <a href="/">My Tournaments</a>
-              <a href="/">Friends</a>
-              <a href="/">My Forum</a>
-              <a href="/">Add Forum</a>
-              <a href="/Forum-Page">Forum</a>
-              <a href="/">Contact</a>
-              <a href="/">Support</a>
-            </Navigation>
-        </Drawer>
-        <Content>
-            <div className="page-content">
-              <Router>
-                <Home path="/" />
-                <HomeReturnUser path="/1" />
-                <ForumPage path="/Forum-Page"/>
-                <LoginRegistration path="/LoginRegistration"/>
-              </Router>
-            </div>
-        </Content>
-    </Layout>
-</div>
+      <Router>
+        <Home path="/" />
+        <HomeReturnUser path="/1" />
+        <ForumPage path="/Forum-Page"/>
+        <LoginRegistration path="/LoginRegistration"/>
+      </Router>
+    </div>
   );
 }
 
