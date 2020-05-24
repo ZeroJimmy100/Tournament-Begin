ab5857019bec23c66596a439e925e50baa730f1b
Daniel <dslee1993@gmail.com>
2020-05-24 02:46:57 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ import Navbar from './components/NavBar';
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Navbar/>
       <Router>
         <Home path="/" />
         <HomeReturnUser path="/1" />
