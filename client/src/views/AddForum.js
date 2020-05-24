0f7e9a47faa630908f06634070731d10f3568cf5
ZeroJimmy100 <61480298+ZeroJimmy100@users.noreply.github.com>
2020-05-24 02:18:02 -0700

    Update AddForum.js

--- client/src/views/AddForum.js
+++ client/src/views/AddForum.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import NavBar from '../components/NavBar';
+import '../components/css/AddForum.css';
 
 export default () => {
     return (
         <div>
+            <div>
+                <NavBar/>
+            </div>
+            
+            <h1>Add Forum</h1>
+
             
         </div>
     )
