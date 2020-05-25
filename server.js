3c1d9f29a21307a934c481ff56286d100aa6a230
Daniel <dslee1993@gmail.com>
2020-05-24 21:39:31 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const port = 8000;
-    
-app.listen(port, () => console.log(`Listening on port: ${port}`) );
+require('./server/config/mongoose.config'); // This is new
+app.use(cors());
+app.use(express.json()); // This is new
+app.use(express.urlencoded({ extended: true })); // This is new
+require('./server/routes/social.routes')(app);
+app.listen(8000, () => {
+    console.log("Listening at Port 8000")
+})
