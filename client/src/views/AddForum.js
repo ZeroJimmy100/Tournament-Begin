e716a506f7c7ed9588b1d5a07d4d137435e7be21
ZeroJimmy100 <61480298+ZeroJimmy100@users.noreply.github.com>
2020-05-25 00:00:24 -0700

    Update AddForum.js

--- client/src/views/AddForum.js
+++ client/src/views/AddForum.js
@@ -6,9 +6,67 @@ export default () => {
     return (
         <div className="category-tabs">
             
-            <h1>Add Forum</h1>
-
+            <div className="AddForumcontainer">
+                <h2 className="CreateTournament">Create your Tournament</h2>
+                <div className="row100">
+                    <div className="col">
+                        <div className="inputBox">
+                            <input required="required" type="text" name="tournamentName" />
+                            <span className="text">Tournament Name</span>
+                            <span className="line"></span>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="inputBox">
+                            <input required="required" type="text" name="gameCate" />
+                            <span className="text">Game</span>
+                            <span className="line"></span>
+                        </div>
+                    </div>
+                </div>
+                    <div className="row100">
+                        <div className="col">
+                            <div className="inputBox">
+                                <input required="required" type="text" name="contestants" />
+                                <span className="text">Contestants</span>
+                                <span className="line"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row100">
+                        <div className="col">
+                            <div className="inputBox">
+                                <input type="date" name="startDate" />
+                                <span className="text">Start Date</span>
+                                <span className="line"></span>
+                            </div>
+                        </div>
+                        <div className="col">
+                            <div className="inputBox">
+                                <input type="date" name="endDate" />
+                                <span className="text">End Date</span>
+                                <span className="line"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row100">
+                        <div className="col">
+                            <div className="inputBox textarea">
+                                <textarea required="required" name="desc"></textarea>
+                                <span className="text">Description</span>
+                                <span className="line"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row100">
+                        <div class="col">
+                            <input type="submit" value="Create"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
             
-        </div>
+            
+     
     )
 }
